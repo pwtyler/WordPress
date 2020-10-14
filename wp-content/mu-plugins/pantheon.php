c34d560b65334ef4931362b2a283e12a3a3d47f6
Doug Cone <nullvariable@users.noreply.github.com>
2020-10-14 08:31:37 -0400

    wp coding standards + PANTHEON_UPDATE_ACCESS constant

--- wp-content/mu-plugins/pantheon.php
+++ wp-content/mu-plugins/pantheon.php
@@ -1,17 +1,21 @@
 <?php
-/*
-  Plugin Name: Pantheon
-  Plugin URI: https://pantheon.io/
-  Description: Building on Pantheon's and WordPress's strengths, together.
-  Version: 0.1
-  Author: Pantheon
-  Author URI: https://pantheon.io/
-*/
+/**
+ * Plugin Name: Pantheon
+ * Plugin URI: https://pantheon.io/
+ * Description: Building on Pantheon's and WordPress's strengths, together.
+ * Version: 0.1
+ * Author: Pantheon
+ * Author URI: https://pantheon.io/
+ *
+ * @package pantheon
+ */
 
 if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
 
-require_once( 'pantheon/pantheon-page-cache.php' );
-require_once( 'pantheon/pantheon-updates.php' );
-require_once( 'pantheon/pantheon-login-form-mods.php' );
+	require_once 'pantheon/pantheon-page-cache.php';
+	if ( ! defined( 'PANTHEON_UPDATE_NOTICES' ) || PANTHEON_UPDATE_ACCESS ) {
+		require_once 'pantheon/pantheon-updates.php';
+	}
+	require_once 'pantheon/pantheon-login-form-mods.php';
 
-endif; # Ensuring that this is on Pantheon
+endif; // Ensuring that this is on Pantheon.
