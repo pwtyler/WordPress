11d623b692043f8765c6eca6f8afbbbaf417cbd6
Doug Cone <nullvariable@users.noreply.github.com>
2020-10-14 08:33:00 -0400

    typo

--- wp-content/mu-plugins/pantheon.php
+++ wp-content/mu-plugins/pantheon.php
@@ -13,7 +13,7 @@
 if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
 
 	require_once 'pantheon/pantheon-page-cache.php';
-	if ( ! defined( 'PANTHEON_UPDATE_NOTICES' ) || PANTHEON_UPDATE_ACCESS ) {
+	if ( ! defined( 'PANTHEON_UPDATE_NOTICES' ) || PANTHEON_UPDATE_NOTICES ) {
 		require_once 'pantheon/pantheon-updates.php';
 	}
 	require_once 'pantheon/pantheon-login-form-mods.php';
