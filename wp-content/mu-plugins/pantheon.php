cfa70c8caf7d063ecea94e8f80ffc6e82feee07b
Phil Tyler <phil.tyler@pantheon.io>
2020-10-14 11:16:57 -0400

    curly brace if block

--- wp-content/mu-plugins/pantheon.php
+++ wp-content/mu-plugins/pantheon.php
@@ -10,12 +10,11 @@
  * @package pantheon
  */
 
-if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
+if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
 
 	require_once 'pantheon/pantheon-page-cache.php';
 	if ( ! defined( 'PANTHEON_UPDATE_NOTICES' ) || PANTHEON_UPDATE_NOTICES ) {
 		require_once 'pantheon/pantheon-updates.php';
 	}
 	require_once 'pantheon/pantheon-login-form-mods.php';
-
-endif; // Ensuring that this is on Pantheon.
+} // Ensuring that this is on Pantheon.
