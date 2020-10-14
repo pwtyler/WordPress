4042fbc39ecf6de0042775efd1e490e83ea46b67
Doug Cone <nullvariable@users.noreply.github.com>
2020-10-14 08:31:37 -0400

    Add PANTHEON_UPDATE_NOTICES constant to disable Pantheon update notice filtering

--- wp-content/mu-plugins/pantheon.php
+++ wp-content/mu-plugins/pantheon.php
@@ -1,17 +1,20 @@
 <?php
-/*
-  Plugin Name: Pantheon
-  Plugin URI: https://pantheon.io/
-  Description: Building on Pantheon's and WordPress's strengths, together.
-  Version: 0.1
-  Author: Pantheon
-  Author URI: https://pantheon.io/
-*/
+/**
+ * Plugin Name: Pantheon
+ * Plugin URI: https://pantheon.io/
+ * Description: Building on Pantheon's and WordPress's strengths, together.
+ * Version: 0.1
+ * Author: Pantheon
+ * Author URI: https://pantheon.io/
+ *
+ * @package pantheon
+ */
 
-if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) :
+if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
 
-require_once( 'pantheon/pantheon-page-cache.php' );
-require_once( 'pantheon/pantheon-updates.php' );
-require_once( 'pantheon/pantheon-login-form-mods.php' );
-
-endif; # Ensuring that this is on Pantheon
+	require_once 'pantheon/pantheon-page-cache.php';
+	if ( ! defined( 'PANTHEON_UPDATE_NOTICES' ) || PANTHEON_UPDATE_NOTICES ) {
+		require_once 'pantheon/pantheon-updates.php';
+	}
+	require_once 'pantheon/pantheon-login-form-mods.php';
+} // Ensuring that this is on Pantheon.
